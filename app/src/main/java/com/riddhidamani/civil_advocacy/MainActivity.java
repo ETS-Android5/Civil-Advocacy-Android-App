96d75e72c841c1aaa478817f95353ab3da327f83
Riddhi Damani <riddhidamani90@gmail.com>
2021-11-06 22:16:51 -0500

    Added Menu Items

--- app/src/main/java/com/riddhidamani/civil_advocacy/MainActivity.java
+++ app/src/main/java/com/riddhidamani/civil_advocacy/MainActivity.java
@@ -2,7 +2,10 @@ package com.riddhidamani.civil_advocacy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu_items, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.about:
+                // code for about activity
+            case R.id.search:
+                // code for search
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
