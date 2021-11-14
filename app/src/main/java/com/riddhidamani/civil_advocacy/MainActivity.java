16a0d1b4910705676abd6019d79ca0b902d6beb4
Riddhi Damani <riddhidamani90@gmail.com>
2021-11-13 22:52:27 -0600

    Added Search Dialog Logic

--- app/src/main/java/com/riddhidamani/civil_advocacy/MainActivity.java
+++ app/src/main/java/com/riddhidamani/civil_advocacy/MainActivity.java
@@ -1,12 +1,18 @@
 package com.riddhidamani.civil_advocacy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,10 +38,40 @@ public class MainActivity extends AppCompatActivity {
             openAboutActivity();
             return true;
         }
+        else if(menuItem.getItemId() == R.id.search) {
+            setManualLocation();
+            return true;
+        }
         else {
             Log.d(TAG, "onOptionsItemSelected: Unknown Item: " + menuItem.getTitle());
         }
         return super.onOptionsItemSelected(menuItem);
+    }
+
+    // When search icon is clicked, Dialog Pops up!
+    private void setManualLocation() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enter a City, State, or a Zip Code:");
+        final EditText manualLoc = new EditText(this);
+        manualLoc.setGravity(Gravity.CENTER_HORIZONTAL);
+        manualLoc.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(manualLoc);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // nothing to cancel the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 
     // When info icon is clicked, About Activity is opened
